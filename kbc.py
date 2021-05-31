f90f41617c980155ac90cac77662aa8d7f158d3d
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-06-01 01:13:50 +0530

    KBC game made

--- kbc.py
+++ kbc.py
@@ -10,8 +10,9 @@ def isAnswerCorrect(question, answer):
         True if the answer is correct
         False if the answer is incorrect
     '''
-
-    return True if answer == 2 else False      #remove this
+    if question['answer'] == answer:
+        return 1
+    return 0
 
 
 def lifeLine(ques):
@@ -20,6 +21,20 @@ def lifeLine(ques):
     :param ques: The question for which the lifeline is asked for. (Type JSON)
     :return: delete the key for two incorrect options and return the new ques value. (Type JSON)
     '''
+
+    from random import choice
+    print(f'\tQuestion {ques+1}: {QUESTIONS[ques]["name"]}' )
+    print(f'\t\tOptions:')
+    correct=QUESTIONS[ques]['answer']
+    l=[1,2,3,4]
+    l.remove(correct)
+    x=choice(l)
+    if correct>x:
+        print(f"\t\t\tOption {x}: {QUESTIONS[ques]['option'+str(x)]}")
+        print(f"\t\t\tOption {correct}: {QUESTIONS[ques]['option'+str(correct)]}")
+    else:
+        print(f"\t\t\tOption {correct}: {QUESTIONS[ques]['option'+str(correct)]}")
+        print(f"\t\t\tOption {x}: {QUESTIONS[ques]['option'+str(x)]}")
 
 
 def kbc():
@@ -43,30 +58,61 @@ def kbc():
         * If the user inputs "quit" (case insensitive) as input, then user returns with the amount he has won until now,
             instead of the minimum amount.
     '''
-
+    total_money = 0
+    money_won = 0
+    lifeline_valid = 1
     #  Display a welcome message only once to the user at the start of the game.
     #  For each question, display the prize won until now and available life line.
     # For now, the below code works for only one question without LIFE-LINE and QUIT checks
+    for i in range(0,15):
+        print(f'\tQuestion {i+1}: {QUESTIONS[i]["name"]}' )
+        print(f'\t\tOptions:')
+        print(f'\t\t\tOption 1: {QUESTIONS[i]["option1"]}')
+        print(f'\t\t\tOption 2: {QUESTIONS[i]["option2"]}')
+        print(f'\t\t\tOption 3: {QUESTIONS[i]["option3"]}')
+        print(f'\t\t\tOption 4: {QUESTIONS[i]["option4"]}')
+        ans = input('Your choice ( 1-4 ) : ')
 
-    print(f'\tQuestion 1: {QUESTIONS[0]["name"]}' )
-    print(f'\t\tOptions:')
-    print(f'\t\t\tOption 1: {QUESTIONS[0]["option1"]}')
-    print(f'\t\t\tOption 2: {QUESTIONS[0]["option2"]}')
-    print(f'\t\t\tOption 3: {QUESTIONS[0]["option3"]}')
-    print(f'\t\t\tOption 4: {QUESTIONS[0]["option4"]}')
-    ans = input('Your choice ( 1-4 ) : ')
+        if ans.lower() == 'lifeline' and lifeline_valid and i != 14:
+            lifeLine(i)
+            lifeline_valid = 0
+            while ans == "lifeline":
+                print('No lifeline left')
+                ans = input('Your choice ( 1-2) : ')
+                if ans.lower() == 'quit':
+                    break
+        elif ans.lower() == 'lifeline' and lifeline_valid == False and i != 14:
+            while ans == "lifeline":
+                print('No lifeline left')
+                ans = input('Your choice ( 1-4) : ')
+                if ans.lower() == 'quit':
+                    break
 
-    # check for the input validations
 
-    if isAnswerCorrect(QUESTIONS[0], int(ans) ):
-        # print the total money won.
-        # See if the user has crossed a level, print that if yes
-        print('\nCorrect !')
 
-    else:
-        # end the game now.
-        # also print the correct answer
-        print('\nIncorrect !')
+        # check for the input validations
+
+        if ans.lower() == 'quit':
+            print("Correct Answer: Option" , QUESTIONS[i]['answer'])
+            print("Total Money Won: ", money_won)
+            break
+
+        if isAnswerCorrect(QUESTIONS[i], int(ans) ):
+            total_money = QUESTIONS[i]['money']
+            if i == 4:
+                money_won = 10000
+            elif i == 9:
+                money_won = 320000
+            print('\nCorrect !')
+
+        else:
+            # also print the correct answer
+            print('\nIncorrect !')
+            print("Correct Answer: Option" , QUESTIONS[i]['answer'])
+            print("Total Money Won: ", money_won)
+            break
+
+        print("Money Won",total_money)
 
     # print the total money won in the end.
 
